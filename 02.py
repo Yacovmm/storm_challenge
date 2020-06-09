729ce4532bd02358e58904d3c4569f8d3de6d5de
yacovmm <yacov94@yahoo.com>
2020-06-08 23:42:47 -0300

    New: questao 02 done

---
+++ 02.py
@@ -0,0 +1,44 @@
+'''
+{[()]} SIM
+{[(])} NAO
+{{[[(())]]}} SIM
+'''
+
+
+def isBalancedSequecy(sequency):
+
+    # Declare opened bracket types
+    openedBrackets = ["(", "{", "["]
+    brackets_dict = {"(" : ")", "{" : "}", "[" : "]", }
+
+    # Declare our stakc
+    stack = []
+
+    # Loop through our sequecy
+    for char in sequency:
+        if char in brackets_dict.keys():
+            stack.append(char)
+        else:
+            # Check if I have something in my stack
+            if not stack:
+                return False
+
+            # Check if the current bracket closing with the last open one
+            if char is not brackets_dict[stack[-1]]:
+                return False
+            stack.pop()
+    
+    # Check if remain any oppening bracket in my stack
+    if stack:
+        return False
+
+    return True
+
+ 
+print(isBalancedSequecy('{[()]}')) # SIM            
+print(isBalancedSequecy('{[(])}')) # NAO            
+print(isBalancedSequecy('{{[[(())]]}}')) # SIM            
+
+
+    
+    
